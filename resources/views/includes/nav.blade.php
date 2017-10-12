6c7e51347f9ff12d770ba3e211741cb5bdc96159
Thang Truong <30461254+thanggtruongg@users.noreply.github.com>
2017-10-12 13:07:00 +1100

    change rate

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -28,7 +28,7 @@
 
                 <li><a href="{{ url('/') }}">Home</a></li> |
                 <li><a href="{{ url('/about') }}">About</a></li> |
-                 <li><a href="{{ url('http://lab3v5.appspot.com/') }}">Rate Trade Centre</a></li>|
+                 <li><a href="{{ url('http://lab3v5.appspot.com/') }}">New Advertisement</a></li>|
 
                 <!-- @if(Auth::guest() || Auth::user()->hasRole("Personal") || Auth::user()->hasRole('Admin')) -->
                     <li><a href="{{ url('/advertisements') }}">Advertisements</a></li> |
